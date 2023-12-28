4f371280180cc986032b62bd14e91ecc6acb114e
mgulrajani <muskaangulrajani@hotmail.com>
2023-12-28 12:11:49 +0530

    initial commit

---
+++ database/db.js
@@ -0,0 +1,6 @@
+module.exports = {
+//  db: 'mongodb://localhost:27017/suredb'
+ // db: 'mongodb://mernsms:qxaPSiychwHek4bCchBIUA7QN3apL5xLzpH6oY7jCA8pfogjUfVG49bQQ8IOwWOVRhvQFIVEazByYATLnaomtg==@mernsms.mongo.cosmos.azure.com:10255/mern-sms?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@mernsms@'
+
+    db: "mongodb+srv://mkg:ZoYRRXm3pFRz9goj@cluster0.cxy5yhu.mongodb.net/sms?retryWrites=true&w=majority"
+};
